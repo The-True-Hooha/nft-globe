3026793230974c279d9f01d882b2c071607c57cb
Davidhero <owogogahhero@outlook.com>
2024-03-04 13:22:29 +0100

    new changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import SmoothScroll from "@/lib/smooth-scroll";
 import GlobeMain from "@/components/3d-globe";
+import GlobeMain2 from "@/components/get-all-light";
+import GlobeWithMakers from "@/components/globe-markers";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <GlobeMain/>
+      <GlobeWithMakers/>
       {/* // <SmoothScroll> */}
-      <main></main>
+      {/* <main></main> */}
     </div>
     // </SmoothScroll>
   );
