9e5acd9bca5c10339e03d889a01fc676fd5ed61b
Davidhero <owogogahhero@outlook.com>
2024-03-05 21:55:28 +0100

    add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        octaBrain: ["OctaBrain", "sans-serif"],
+        silkscreen: ["Silkscreen", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate",)],
 };
 export default config;
