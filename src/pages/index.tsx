f78688e9ca52d5077fce0c653504e0c7f4b3df3f
Davidhero <owogogahhero@outlook.com>
2024-03-25 19:23:09 +0100

    new changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,33 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import SmoothScroll from "@/lib/smooth-scroll";
-import GlobeMain from "@/components/3d-globe";
-import GlobeMain2 from "@/components/get-all-light";
-import GlobeWithMakers from "@/components/globe-markers";
 
-// const inter = Inter({ subsets: ["latin"] });
+import GlobeWithMakers from "@/components/globe-markers";
+import { useNavigationEvent } from "@/components/component/use-nav";
+import LoaderUI from "@/components/component/loader";
+import { lazy, Suspense, useEffect, useState } from "react";
+
+const LazyLoad = lazy(() => import('../components/globe-markers'))
 
 export default function Home() {
+//   const [loading, setLoading] = useState(true);
+//  useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//     }, 500);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   if (loading) {
+//     return <LoaderUI />;
+//   }
+  // useNavigationEvent(() => setLoading(false));
   return (
     <div>
-      <GlobeWithMakers/>
-      {/* // <SmoothScroll> */}
-      {/* <main></main> */}
+      {/* {loading && <LoaderUI /> } */}
+      <Suspense fallback={<LoaderUI />}>
+        <LazyLoad />
+      </Suspense>
+
+      {/* <LoaderUI /> */}
     </div>
-    // </SmoothScroll>
   );
 }
